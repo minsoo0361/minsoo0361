1774d7a99f010223b672e282a0f3d7340c1387a1
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-02-11 21:40:24 +0900

    [Bronze III] Title: 네 번째 점, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,12 @@
+x_lst = []
+y_lst = []
+for i in range(3):
+    x, y = map(int, input().split())
+    x_lst.append(x)
+    y_lst.append(y)
+for k in x_lst:
+    if x_lst.count(k) == 1:
+        print(k, end = ' ')
+for j in y_lst:
+    if y_lst.count(j) == 1:
+        print(j)
