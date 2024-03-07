00cd5d02d9c9fc0af6d28af1d58209cfb6097d08
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-03-07 23:16:45 +0900

    good

--- 백준/2실버/2108통계학.py
+++ 백준/2실버/2108통계학.py
@@ -1,15 +1,29 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 arr = []
 cnt = 0
-lst = [0] * 8001
 for _ in range(N):
-    nm = int(input())
-    arr.append(nm)
+    arr.append(int(input()))
 arr.sort()
-a = sum(arr) // N
-b = arr[N//2]
-for i in range (len(lst)):
+print(round(sum(arr) / N))
+print(arr[N//2])
+dic = dict()
+for i in arr:
+    if i in dic:
+        dic[i] += 1
+    else:
+        dic[i] = 1
+mx = max(dic.values())
+mx_lst = []
+for i in dic:
+    if mx == dic[i]:
+        mx_lst.append(i)
+if len(mx_lst) > 1:
+    print(mx_lst[1])
+elif len(mx_lst) == 1:
+    print(mx_lst[0])
+print(max(arr)-min(arr))
 
 
 
-print(lst)
