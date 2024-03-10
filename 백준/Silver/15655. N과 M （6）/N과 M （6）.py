bb4db8243d88efa37b1f0b4a50247f6cf2514fc7
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-03-11 01:08:58 +0900

    [Silver III] Title: N과 M (6), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+def back(n):
+    if len(lst) == M:
+        print(*lst)
+        return
+
+    for i in range(n, N):
+        if arr[i] not in lst:
+            lst.append(arr[i])
+            back(i + 1)
+            lst.pop()
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+lst = []
+
+back(0)
