52a354085800918bebc4c4ad5af06b004ed4b982
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-03-17 17:19:36 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 111240 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,14 +2,17 @@ import sys
 input = sys.stdin.readline
 
 def dfs(x, y):
-    visited[x][y] = True
-    dx = [0, 0, -1, 1]
-    dy = [1, -1, 0, 0]
-    for k in range(4):
-        nx = x + dx[k]
-        ny = y + dy[k]
-        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and arr[nx][ny]== 1:
-            dfs(nx, ny)
+    stack = [[x, y]]
+    while stack:
+        x, y = stack.pop()
+        visited[x][y] = True
+        dx = [0, 0, -1, 1]
+        dy = [1, -1, 0, 0]
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and arr[nx][ny] == 1:
+                stack.append([nx, ny])
 
 
 T = int(input())
