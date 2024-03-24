151224bbc6c9c2c6c42f3a8a8ef5e2f65a0f194e
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-03-24 19:20:45 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 823 ms, Memory: 153,376 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,19 +1,24 @@
+def dfs(cnt, sum_height):
+    # 기저 조건
+    if sum_height >= B:
+        ans.append(sum_height)
+        return
+
+    if cnt == N:
+        return
+
+    # 쌓을 때
+    dfs(cnt+1, sum_height+height[cnt])
+
+    # 안쌓을때
+    dfs(cnt+1, sum_height)
+
+
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, B = map(int, input().split())
     height = list(map(int, input().split()))
-    lst = []
-    for i in range(1 << N):
-        subset = []
-        for j in range(N):
-            if i & (1<<j):
-                subset.append(height[j])
-        lst.append(sum(subset))
-    nm_lst = sorted(set(lst))
-    for i in nm_lst:
-        if i >= B:
-            print(f'#{tc} {i-B}')
-            break
+    ans = []
 
-
-
+    dfs(0, 0)
+    print(f'#{tc} {min(ans)-B}')
