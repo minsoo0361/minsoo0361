4148d5c496e257b0f039c41ce16fcffb067257b3
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-07-17 23:11:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,9 +1,11 @@
 N = int(input())
-
-for cnt_5 in range(N//5, -1, -1):
-    cnt_3 = (N - cnt_5*5)//3
-    if cnt_5*5 + cnt_3 * 3 ==N:
-        print(cnt_5 + cnt_3)
+answer = 300000
+for i in range(5000):
+    if N < i * 5:
         break
-else:
-    print(-1)
+    tmp = N - (i * 5)
+    if (tmp % 3 == 0):
+        answer = min(answer, i + (tmp // 3))
+if answer == 300000:
+    answer = -1
+print(answer)
