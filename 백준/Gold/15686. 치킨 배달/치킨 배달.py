06a28181c853eec608216e0a14f638bdb24bb6e4
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-07-27 20:24:47 +0900

    [Gold V] Title: 치킨 배달, Time: 192 ms, Memory: 113980 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,35 +1,36 @@
 def dfs(idx):
-    global answer
+    global ans
     if idx >= len(chicken):
-        if len(now_chicken) != M:
+        if len(real_chicken) != M:
             return
+        
         sum = 0
         for i in range(len(house)):
-            minimun_size = 3000
-            for k in range(len(now_chicken)):
-                minimun_size = min(minimun_size, abs(house[i][0] - now_chicken[k][0]) + abs(house[i][1] - now_chicken[k][1]))
-            sum += minimun_size
-
-        answer = min(answer, sum)
+            min_dist = 3000
+            for k in range(len(real_chicken)):
+                min_dist = min(min_dist, abs(house[i][0] - real_chicken[k][0]) + abs(house[i][1] - real_chicken[k][1]))
+            
+            sum += min_dist
+        ans = min(ans, sum)
         return
 
-    now_chicken.append(chicken[idx])
+    real_chicken.append(chicken[idx])
     dfs(idx+1)
-    now_chicken.pop()
-    dfs(idx+1)
+    real_chicken.pop()
+    dfs(idx+1)    
 
 N, M = map(int, input().split())
-arr = [list(map(int, input().split()))for _ in range(N)]
-answer = 9999
+arr = [list(map(int, input().split())) for _ in range(N)]
 house = []
 chicken = []
-now_chicken = []
+real_chicken = []
+ans = 9999
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 1:
             house.append([i, j])
         if arr[i][j] == 2:
             chicken.append([i, j])
-dfs(0)
-print(answer)
 
+dfs(0)
+print(ans)
