acc3d52367e73f6517f2d667096d4ef4043a5bbc
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2024-09-07 14:46:13 +0900

    [Silver IV] Title: 수 찾기, Time: 216 ms, Memory: 136240 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,12 +1,28 @@
-import sys
-input = sys.stdin.readline
-input()
-arr = list(set(map(int, input().split())))
-input()
-nm = list(map(int, input().split()))
-arr.sort()
-for i in nm:
-    if i in arr:
+N = int(input())
+N_lst = list(map(int, input().split()))
+N_lst.sort()
+M = int(input())
+M_lst = list(map(int, input().split()))
+
+    
+def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
+    
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return True
+        if target < arr[mid]:
+            end = mid - 1
+        if target > arr[mid] : 
+            start = mid + 1
+    return False
+
+for i in range(M):
+    if binary_search(N_lst, M_lst[i]):
         print(1)
     else:
         print(0)
+
+
