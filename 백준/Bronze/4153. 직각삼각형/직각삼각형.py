8475eb0f08ec8f2bf60fbfcd59a09b17a970766b
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-02-01 14:29:01 +0900

    [Bronze III] Title: 직각삼각형, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True:
+    arr = list(map(int, input().split()))
+    arr.sort()
+    if arr[0] == arr[1] == arr[2] == 0:
+        break
+    
+    if arr[2] ** 2 == arr[0] ** 2 + arr[1] ** 2:
+        print("right")
+    else:
+        print("wrong")
