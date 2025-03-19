b9b65c66710cc484de1bdb749644f65a407eff41
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-03-19 23:06:04 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1392 ms, Memory: 287556 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,40 @@
+from collections import deque
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs():
+    queue = deque()
+    queue.append((0, 0, K))
+    visited[0][0][K] = 0
+
+    while queue:
+        x, y, wall_left = queue.popleft()
+        if x == N-1 and y == M -1:
+            return visited[x][y][wall_left] + 1
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0 and visited[nx][ny][wall_left] == -1:
+                visited[nx][ny][wall_left] = visited[x][y][wall_left] + 1
+                queue.append((nx, ny, wall_left))
+                
+        if wall_left > 0:
+            for k in range(4):
+                nx = x + dx[k]
+                ny = y + dy[k]
+
+                if 0 <= nx < N and 0 <= ny < M and visited[nx][ny][wall_left-1] == -1:
+                    visited[nx][ny][wall_left -1] = visited[x][y][wall_left] + 1
+                    queue.append((nx, ny, wall_left -1))
+    return -1
+
+
+
+N, M = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(N)]
+K = 1
+visited = [[[-1] * (K+1) for _ in range(M)] for _ in range(N)]
+print(bfs())
+
