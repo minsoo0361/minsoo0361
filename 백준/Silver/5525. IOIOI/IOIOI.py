973967a46a42dc5af505047cc2439e96bc7c3e70
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-05-05 20:07:25 +0900

    [Silver I] Title: IOIOI, Time: 160 ms, Memory: 156044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,17 +1,20 @@
 N = int(input())
 M = int(input())
-S = str(input())
-real_num = N * 2 + 1
-arr = 'IOI'
-plus_oi = 'OI'
+S = list(input())
+
 cnt = 0
-if N == 1:
-    for i in range(M):
-        if arr == S[i:i+real_num]:
+i = 0
+pattern_count = 0
+
+while i < M - 1:
+    if S[i] == 'I' and S[i + 1] == 'O' and i + 2 < M and S[i + 2] == 'I':
+        pattern_count += 1
+        i += 2
+        if pattern_count == N:
             cnt += 1
-else:
-    arr = arr + plus_oi * (N-1)
-    for i in range(M):
-        if arr == S[i:i+real_num]:
-            cnt += 1
+            pattern_count -= 1
+    else:
+        pattern_count = 0
+        i += 1
+
 print(cnt)
