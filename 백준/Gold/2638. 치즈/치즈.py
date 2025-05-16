5909cac0715ada6b2e71ac7774597b9196253942
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-05-16 23:59:42 +0900

    [Gold III] Title: 치즈, Time: 196 ms, Memory: 115564 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs():
+    queue = deque()
+    queue.append((0, 0))
+    visited[0][0] = True
+
+    while queue:
+        x, y = queue.popleft()        
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == False:
+                if arr[nx][ny] >= 1:
+                    arr[nx][ny] += 1                    
+                else:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+    melt()
+    
+
+def melt():
+    global cnt
+    global answer
+    melt_cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] >= 3:
+                arr[i][j] = 0
+                melt_cnt += 1
+            elif arr[i][j] == 2:
+                arr[i][j] = 1
+    cnt -= melt_cnt
+    answer += 1
+    
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+cnt = 0
+answer = 0
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 1:
+            cnt += 1
+while True:
+    visited = [[False] * M for _ in range(N)]
+    bfs()
+    if cnt == 0:
+        print(answer)
+        break
