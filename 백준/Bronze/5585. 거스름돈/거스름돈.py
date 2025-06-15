d4ce36cc031d939ff11d189b5b98bfff30fceeed
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-06-15 23:24:08 +0900

    [Bronze II] Title: 거스름돈, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,29 @@
+money = 1000 - int(input())
+answer = 0
+while money > 0:
+    cnt = 0
+    if money >= 500:
+        cnt += money // 500
+        money = money - cnt * 500
+        answer += cnt
+    elif money >= 100:
+        cnt += money // 100
+        money = money - cnt * 100
+        answer += cnt
+    elif money >= 50:
+        cnt += money // 50
+        money = money - cnt * 50
+        answer += cnt
+    elif money >= 10:
+        cnt += money // 10
+        money = money - cnt * 10
+        answer += cnt
+    elif money >= 5:
+        cnt += money // 5
+        money = money - cnt * 5
+        answer += cnt
+    elif money >= 1:
+        cnt += money // 1
+        money = money - cnt * 1
+        answer += cnt
+print(answer)
