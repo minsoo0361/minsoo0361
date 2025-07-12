a6a5f603906d637342cb234cf546831a2dc10924
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-07-12 23:40:50 +0900

    [Bronze III] Title: 주사위 게임, Time: 104 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -1,24 +1,16 @@
 N = int(input())
-answer = []
+lst = []
 for _ in range(N):
-    lst = list(map(int, input().split()))
-    lst.sort(reverse=True)
-    cnt = 0
-    if lst[0] == lst[1]:
-        cnt += 1
-    if lst[1] == lst[2]:
-        cnt += 1
-    if lst[0] == lst[2]:
-        cnt += 1
-
-    if cnt == 0:
-        money = lst[0] * 100
-        answer.append(money)
-    elif cnt == 1:
-        money = (1000 + lst[1] * 100)    
-        answer.append(money)
-    elif cnt == 3:
-        money = (10000 + lst[0] * 1000)
-        answer.append(money)
-
-print(max(answer))
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    if arr[0] == arr[1] == arr[2]:
+        lst.append(10000+arr[0]*1000)
+    elif arr[0] == arr[1]: 
+        lst.append(1000 + arr[0] * 100)
+    elif arr[1] == arr[2]:
+        lst.append(1000 + arr[1] * 100)
+    elif arr[0] == arr[2]:
+        lst.append(1000 + arr[0] * 100)
+    else:
+        lst.append(arr[0] * 100)
+print(max(lst))
