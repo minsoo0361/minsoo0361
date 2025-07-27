bfdb0c88333502f75f821edcd675e3c8ef9a76a1
minsoo0361 <114391609+minsoo0361@users.noreply.github.com>
2025-07-28 00:00:10 +0900

    [Bronze II] Title: KMP는 왜 KMP일까?, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.py
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.py
@@ -1,18 +1,3 @@
-import sys
-
-def solve():
-    s = sys.stdin.readline().strip()
-    result = []
-    
-    # 첫 번째 문자는 항상 추출
-    result.append(s[0])
-    
-    # 하이픈 다음 문자를 추출
-    for i in range(1, len(s)):
-        if s[i-1] == '-':
-            result.append(s[i])
-            
-    print("".join(result))
-
-if __name__ == '__main__':
-    solve()
+name = input().split("-")
+for i in range(len(name)):
+    print(name[i][0], end = "")
